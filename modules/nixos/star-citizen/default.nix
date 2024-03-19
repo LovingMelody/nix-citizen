8281b0096fa92d86d736d8df756a9e5741b05254
Melody Renata <me@lovingmelody.io>
2024-03-19 19:45:34 -0400

    Update module star-citizen:
    Removed options:
      - nix-citizen.starCitizen.setKernelLimits
      - nix-citizen.starCitizen.setLoginLimits
    
    Added options:
      - nix-citizen.starCitizen.location to set the install path
      - nix-citizen.starCitizen.preCommands: run before the script
      - nix-citizen.starCitizen.postCommands: after the script is run
      - nix-citizen.starCitizen.helperScript: exerimental helper
      - nix-citizen.starCitizen.setLimits: set system runtime defaults

--- modules/nixos/star-citizen/default.nix
+++ modules/nixos/star-citizen/default.nix
@@ -8,51 +8,86 @@ in with lib; {
     enable = mkEnableOption "Enable star-citizen";
     package = mkOption {
       description = "Package to use for star-citizen";
+      type = types.package;
       default = if (builtins.hasAttr "star-citizen" pkgs) then
         pkgs.star-citizen
       else
         builtins.trace
         "warning: pkgs does not include star-citizen, (missing overlay?) using nix-citizen's package"
         flake-packages.star-citizen;
+      apply = star-citizen:
+        star-citizen.override {
+          preCommands = ''
+            ${cfg.preCommands}
+            ${if cfg.helperScript.enable then
+              "${cfg.helperScript.package}/bin/star-citizen-helper"
+            else
+              ""}
+          '';
+          inherit (cfg) postCommands location;
+
+        };
     };
-    setKernelLimits = {
+    location = mkOption {
+      default = "$HOME/Games/star-citizen";
+      type = types.str;
+      description = "Path to install star-citizen";
+    };
+    preCommands = mkOption {
+      default = "";
+      type = types.str;
+      description = "Additional commands to be run before star-citizen is run";
+      example = ''
+        export DXVK_HUD=compiler
+        export MANGO_HUD=1
+      '';
+    };
+    postCommands = mkOption {
+      default = "";
+      type = types.str;
+      description = "Additional commands to be run after star-citizen is run";
+    };
+    helperScript = {
       enable = mkOption {
+        default = false;
         type = types.bool;
-        default = true;
-        description = "Set vm.max_map_count and fs.file-max for you";
       };
-      maxMapCount = mkOption {
-        type = types.int;
-        default = 16777216;
-      };
-      fileMax = mkOption {
-        type = types.int;
-        default = 524288;
+      package = mkOption {
+        description = "Package to use for star-citizen-helper";
+        type = types.package;
+        default = if (builtins.hasAttr "star-citizen-helper" pkgs) then
+          pkgs.star-citizen-helper
+        else
+          builtins.trace
+          "warning: pkgs does not include star-citizen-helper, (missing overlay?) using nix-citizen's package"
+          flake-packages.star-citizen-helper;
       };
     };
-    setLoginLimit = {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Set security.pam.loginLimits";
-      };
-      limit = mkOption {
-        type = types.int;
-        default = 16777216;
-      };
+    setLimits = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Configures your system to meet some of the requirements to run star-citizen
+        Set `vm.max_map_count` default to `16777216` (sysctl(8))
+        Set `fs.file-max` default to `524288` (sysctl(8))
+
+        Also sets `security.pam.loginLimits` to increase hard (limits.conf(5))
+
+        Changes outlined in  https://github.com/starcitizen-lug/knowledge-base/wiki/Manual-Installation#prerequisites
+      '';
     };
   };
   config = mkIf cfg.enable {
-    boot.kernel.sysctl = mkIf cfg.setKernelLimits.enable {
-      "vm.max_map_count" = mkOverride 999 cfg.setKernelLimits.maxMapCount;
-      "fs.file-max" = mkOverride 999 cfg.setKernelLimits.fileMax;
+    boot.kernel.sysctl = mkIf cfg.setLimits {
+      "vm.max_map_count" = mkOverride 999 16777216;
+      "fs.file-max" = mkOverride 999 524288;
     };
-    security.pam = mkIf cfg.setLoginLimit.enable {
+    security.pam = mkIf cfg.setLimits {
       loginLimits = [{
         domain = "*";
         type = "soft";
         item = "nofile";
-        value = "${toString cfg.setLoginLimit.limit}";
+        value = "16777216";
       }];
     };
     environment.systemPackages = [ cfg.package ];
