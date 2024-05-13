7c5e11d5a64e07eb9dd19433b46bc3217911c14d
Melody Renata <me@lovingmelody.io>
2024-05-13 14:42:31 -0400

    Chore: update updatescript

--- update-script.sh
+++ update-script.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash
-#! nix-shell -p bash nix-update npins
-nix-update --flake lug-helper
-npins update
+#! nix-shell -p bash nix-update npins curl jq gnused gnugrep
+update-gplasync () {
+  local latest="$(curl 'https://gitlab.com/api/v4/projects/Ph42oN%2Fdxvk-gplasync/repository/tags?per_page=1' | , jq -r ".[0].name" | sed 's/^v//')"
+  local current="$(npins show | grep "version" | sed 's/\s\s\s\sversion: v//')"
+  if [ "$latest" != "$current" ]; then
+    npins add gitlab Ph42oN dxvk-gplasync --at "$latest"
+  fi
+}
+#nix-update --flake lug-helper
+update-gplasync
 nix fmt
