b82bfa07f18e9a6a2932ebf36aa53e12e7eb3e3e
Melody Renata <me@lovingmelody.io>
2024-10-13 01:14:09 -0400

    Update lug-helper: v3.0 -> v3.0.1

--- pkgs/lug-helper/default.nix
+++ pkgs/lug-helper/default.nix
@@ -12,12 +12,12 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   name = "lug-helper";
-  version = "3.0";
+  version = "3.0.1";
   src = fetchFromGitHub {
     owner = "starcitizen-lug";
     repo = "lug-helper";
     rev = "refs/tags/v${finalAttrs.version}";
-    hash = "sha256-EE5GQZp8rEXUx8YXNdRBdnCTT4nFSwLcfDv+q+8L6UA=";
+    hash = "sha256-GL+pPyj3Qz+wmJD59kfGz80LufotPq2KdaLQCkQnCFg=";
   };
 
   buildInputs = [
@@ -45,7 +45,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   postInstall = ''
     install -Dm755 lug-helper.sh $out/bin/lug-helper
-    install -Dm644 lug-logo.png $out/share/pixmaps/lug-logo.png
+    install -Dm644 lug-logo.png $out/share/icons/hicolor/256x256/apps/lug-logo.png
+    install -Dm644 rsi-launcher.png $out/share/icons/hicolor/256x256/apps/rsi-launcher.png
     install -Dm644 lib/* -t $out/share/lug-helper
 
     wrapProgram $out/bin/lug-helper \
