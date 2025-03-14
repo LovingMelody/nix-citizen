be61a6adb70ec0befe85185503f416ac093584af
Melody Renata <me@lovingmelody.io>
2025-03-13 23:48:28 -0400

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/32ea77a06711b758da0ad9bd6a844c5740a87abd?narHash=sha256-7H9XgNiGLKN1G1CgRh0vUL4AheZSYzPm%2BzmZ7vxbJdo%3D' (2025-02-01)
      → 'github:hercules-ci/flake-parts/f4330d22f1c5d2ba72d3d22df5597d123fdb60a9?narHash=sha256-%2Bu2UunDA4Cl5Fci3m7S643HzKmIDAe%2BfiXrLqYsR2fs%3D' (2025-03-07)
    • Updated input 'nix-gaming':
        'github:fufexan/nix-gaming/452fbd6a30f128dddf26c63f727cf685b5af5658?narHash=sha256-KiGmSYyO9MJJqYD7nbQ0NQgsAR2VVY/wRra0Bob4KtE%3D' (2025-02-26)
      → 'github:fufexan/nix-gaming/b2a32ef80ad0cc7f3dee928771791625ff9494c1?narHash=sha256-SU6Q/IBGJ9a7u6WrVjZ%2BShoIjK3To/lD3U37DgfX1Tw%3D' (2025-03-14)
    • Updated input 'nix-gaming/flake-parts':
        'github:hercules-ci/flake-parts/32ea77a06711b758da0ad9bd6a844c5740a87abd?narHash=sha256-7H9XgNiGLKN1G1CgRh0vUL4AheZSYzPm%2BzmZ7vxbJdo%3D' (2025-02-01)
      → 'github:hercules-ci/flake-parts/f4330d22f1c5d2ba72d3d22df5597d123fdb60a9?narHash=sha256-%2Bu2UunDA4Cl5Fci3m7S643HzKmIDAe%2BfiXrLqYsR2fs%3D' (2025-03-07)
    • Updated input 'nix-gaming/flake-parts/nixpkgs-lib':
        'https://github.com/NixOS/nixpkgs/archive/072a6db25e947df2f31aab9eccd0ab75d5b2da11.tar.gz?narHash=sha256-vJzFZGaCpnmo7I6i416HaBLpC%2BhvcURh/BQwROcGIp8%3D' (2025-02-01)
      → 'github:nix-community/nixpkgs.lib/147dee35aab2193b174e4c0868bd80ead5ce755c?narHash=sha256-oiwv/ZK/2FhGxrCkQkB83i7GnWXPPLzoqFHpDD3uYpk%3D' (2025-03-02)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/0196c0175e9191c474c26ab5548db27ef5d34b05?narHash=sha256-WGaHVAjcrv%2BCun7zPlI41SerRtfknGQap281%2BAakSAw%3D' (2025-02-24)
      → 'github:NixOS/nixpkgs/6607cf789e541e7873d40d3a8f7815ea92204f32?narHash=sha256-cPfs8qMccim2RBgtKGF%2Bx9IBCduRvd/N5F4nYpU0TVE%3D' (2025-03-13)

--- flake.nix
+++ flake.nix
@@ -22,87 +22,96 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    self,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      flake-parts,
+      self,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./modules
         ./overlays.nix
         inputs.flake-parts.flakeModules.easyOverlay
         inputs.treefmt-nix.flakeModule
       ];
-      systems = ["x86_64-linux"];
+      systems = [ "x86_64-linux" ];
       flake = {
         githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
           checks =
-            (inputs.nixpkgs.lib.getAttrs ["x86_64-linux"] self.checks)
-            // (inputs.nixpkgs.lib.getAttrs ["x86_64-linux"] self.packages);
+            (inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks)
+            // (inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.packages);
         };
       };
-      perSystem = {
-        config,
-        system,
-        pkgs,
-        ...
-      }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        overlayAttrs = config.packages;
-        packages = let
-          inherit (inputs.nixpkgs.lib) optional warn;
-        in {
-          xwayland-patched = pkgs.xwayland.overrideAttrs (p: {
-            patches =
-              (p.patches or [])
-              ++ optional (!builtins.elem ./patches/ge-xwayland-pointer-warp-fix.patch (p.patches or [])) ./patches/ge-xwayland-pointer-warp-fix.patch;
-          });
-          star-citizen-helper = pkgs.callPackage ./pkgs/star-citizen-helper {};
-          inherit (inputs.nix-gaming.packages.${system}) umu-launcher star-citizen star-citizen-umu;
-          dxvk-gplasync = warn "This package will be removed in a future update and is now just an alias for dxvk" pkgs.dxvk;
-          lug-helper = let
-            pkg = pkgs.callPackage ./pkgs/lug-helper {};
-          in
-            # We only use the local lug-helper if nixpkgs doesn't have it
-            # And if the nixpkgs version isnt newer than local
-            if (builtins.hasAttr "lug-helper" pkgs)
-            then
-              if (inputs.nixpkgs.lib.strings.versionOlder pkg.version pkgs.lug-helper.version)
-              then pkgs.lug-helper
-              else pkg
-            else pkg;
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          overlayAttrs = config.packages;
+          packages =
+            let
+              inherit (inputs.nixpkgs.lib) optional warn;
+            in
+            {
+              xwayland-patched = pkgs.xwayland.overrideAttrs (p: {
+                patches =
+                  (p.patches or [ ])
+                  ++ optional (
+                    !builtins.elem ./patches/ge-xwayland-pointer-warp-fix.patch (p.patches or [ ])
+                  ) ./patches/ge-xwayland-pointer-warp-fix.patch;
+              });
+              star-citizen-helper = pkgs.callPackage ./pkgs/star-citizen-helper { };
+              inherit (inputs.nix-gaming.packages.${system}) umu-launcher star-citizen star-citizen-umu;
+              dxvk-gplasync = warn "This package will be removed in a future update and is now just an alias for dxvk" pkgs.dxvk;
+              lug-helper =
+                let
+                  pkg = pkgs.callPackage ./pkgs/lug-helper { };
+                in
+                # We only use the local lug-helper if nixpkgs doesn't have it
+                # And if the nixpkgs version isnt newer than local
+                if (builtins.hasAttr "lug-helper" pkgs) then
+                  if (inputs.nixpkgs.lib.strings.versionOlder pkg.version pkgs.lug-helper.version) then
+                    pkgs.lug-helper
+                  else
+                    pkg
+                else
+                  pkg;
 
-          inherit (inputs.nix-gaming.packages.${system}) winetricks-git;
-        };
-        treefmt = {
-          # Project root
-          projectRootFile = "flake.nix";
-          # Terraform formatter
-          programs = {
-            yamlfmt.enable = true;
-            nixfmt.enable = true;
-            deno.enable = true;
-            deadnix = {
-              enable = true;
-              # Can break callPackage if this is set to false
-              no-lambda-pattern-names = true;
+              inherit (inputs.nix-gaming.packages.${system}) winetricks-git;
             };
-            statix.enable = true;
-            rustfmt.enable = true;
-            beautysh.enable = true;
-          };
-          settings.formatter = {
-            deadnix.excludes = ["npins/default.nix"];
-            nixfmt.excludes = ["npins/default.nix"];
-            deno.excludes = ["npins/default.nix"];
-            statix.excludes = ["npins/default.nix"];
-            yamlfmt.excludes = ["npins/sources.json"];
+          treefmt = {
+            # Project root
+            projectRootFile = "flake.nix";
+            # Terraform formatter
+            programs = {
+              yamlfmt.enable = true;
+              nixfmt.enable = true;
+              deno.enable = true;
+              deadnix = {
+                enable = true;
+                # Can break callPackage if this is set to false
+                no-lambda-pattern-names = true;
+              };
+              statix.enable = true;
+              rustfmt.enable = true;
+              beautysh.enable = true;
+            };
+            settings.formatter = {
+              deadnix.excludes = [ "npins/default.nix" ];
+              nixfmt.excludes = [ "npins/default.nix" ];
+              deno.excludes = [ "npins/default.nix" ];
+              statix.excludes = [ "npins/default.nix" ];
+              yamlfmt.excludes = [ "npins/sources.json" ];
+            };
           };
         };
-      };
     };
 }
