9bdec5a7df35e8f666ba41a2a025b0b3366edaba
Melody Renata <me@lovingmelody.io>
2025-07-19 17:11:53 -0400

    rebase wine-unstable

--- pkgs/wine-astral/default.nix
+++ pkgs/wine-astral/default.nix
@@ -3,34 +3,15 @@ let
   # BROKEN_LUG_WINE_PATCHES_COMMIT = "98d6a9b6ce102726030bec3ee9ff63e3fad59ad5";
 in
   {
-    inputs,
     lib,
     pins,
     pkgs,
-    pkgsCross,
-    pkgsi686Linux,
-    callPackage,
-    moltenvk,
-    overrideCC,
-    wrapCCMulti,
-    gcc14,
-    stdenv,
     linuxHeaders,
     linuxPackages_latest,
     fetchurl,
-    wine-mono,
     ntsync ? lib.versionAtLeast linuxHeaders.version MIN_KERNEL_VERSION_NTSYNC,
+    ...
   }: let
-    supportFlags = import ./supportFlags.nix;
-    nixpkgs-wine = builtins.path {
-      path = inputs.nixpkgs;
-      name = "source";
-      filter = path: type: let
-        wineDir = "${inputs.nixpkgs}/pkgs/applications/emulators/wine/";
-      in
-        (type == "directory" && (lib.hasPrefix path wineDir))
-        || (type != "directory" && (lib.hasPrefix wineDir path));
-    };
     updatedHeaders =
       if (lib.versionAtLeast linuxHeaders.version MIN_KERNEL_VERSION_NTSYNC)
       then linuxHeaders
@@ -54,49 +35,25 @@ in
             - Update your pinned nixpkgs
             - wine-astral.override { ntsync = false; }
         '';
-
-    base = let
-      sources = (import "${inputs.nixpkgs}/pkgs/applications/emulators/wine/sources.nix" {inherit pkgs;}).unstable;
-    in {
-      inherit supportFlags moltenvk;
-      buildScript = "${nixpkgs-wine}/pkgs/applications/emulators/wine/builder-wow.sh";
-      configureFlags = ["--disable-tests"];
-      geckos = with sources; [gecko32 gecko64];
-      mingwGccs = with pkgsCross; [mingw32.buildPackages.gcc14 mingwW64.buildPackages.gcc14];
-      monos = [wine-mono];
-      pkgArches = [pkgs pkgsi686Linux];
-      platforms = ["x86_64-linux"];
-      stdenv = overrideCC stdenv (wrapCCMulti gcc14);
-      wineRelease = "unstable";
-    };
   in
-    (callPackage "${nixpkgs-wine}/pkgs/applications/emulators/wine/base.nix"
-      (lib.recursiveUpdate base rec {
-        pname = "wine-astral-full";
-        version = lib.removeSuffix "\n" (lib.removePrefix "Wine version " (builtins.readFile "${src}/VERSION"));
-        src =
-          if ntsync
-          then pins.wine-tkg-ntsync
-          else pins.wine-tkg;
-        patches = let
-          blacklist = [
-            "10.2+_eac_fix.patch"
-            "winewayland-no-enter-move-if-relative.patch"
-            "hidewineexports.patch"
-            "reg_show_wine.patch"
-            # "cache-committed-size.patch"
+    pkgs.wineWow64Packages.unstable.overrideAttrs (old: {
+      pname = "wine-astral-full";
+      patches = let
+        blacklist = [
+          "10.2+_eac_fix.patch"
+          "winewayland-no-enter-move-if-relative.patch"
+          # "cache-committed-size.patch"
+        ];
+        filter = name: _type: ! (builtins.elem (builtins.baseNameOf name) blacklist);
+        cleanedPatches = builtins.filterSource filter "${pins.lug-patches}/wine";
+        lug-patches = builtins.attrNames (builtins.readDir cleanedPatches);
+        patches =
+          map (f: "${cleanedPatches}/${f}") lug-patches
+          ++ lib.optionals ntsync [
+            "${pins.wine-tkg-git}/wine-tkg-git/wine-tkg-patches/misc/fastsync/ntsync5-mainline.patch"
           ];
-          filter = name: _type: ! (builtins.elem (builtins.baseNameOf name) blacklist);
-          cleanedPatches = builtins.filterSource filter "${pins.lug-patches}/wine";
-          lug-patches = builtins.attrNames (builtins.readDir cleanedPatches);
-          patches =
-            map (f: "${cleanedPatches}/${f}") lug-patches
-            ++ [
-              ./hideWineExports.patch
-            ];
-        in
-          patches;
-      })).overrideAttrs (old: {
+      in
+        old.patches ++ patches;
       passthru.ntsync-enabled = ntsync;
       postPatch = ''
         ${old.postPatch or ""}
