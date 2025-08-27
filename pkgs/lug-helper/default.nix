e0ceb936aad14c4ebedbba591f77bc1076a264f6
Melody Renata <me@lovingmelody.io>
2025-08-27 07:59:02 -0400

    lug-helper: 4.3 -> 4.4

--- pkgs/lug-helper/default.nix
+++ pkgs/lug-helper/default.nix
@@ -15,12 +15,12 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   name = "lug-helper";
-  version = "4.3";
+  version = "4.4";
   src = fetchFromGitHub {
     owner = "starcitizen-lug";
     repo = "lug-helper";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-x3kM9EZtoMLt5MsZZQPOo/vd/1Tsf4QccwwghvWCZG8=";
+    hash = "sha256-wYNYgwcves9nmUccDQPGekVt3BIB4QE/t6l9vUwXYUs=";
   };
 
   buildInputs = [
