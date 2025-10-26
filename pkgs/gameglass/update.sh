ddd02be3bbe950237cf07310f9e8d8bbfbea54ad
Melody Renata <me@lovingmelody.io>
2025-10-26 00:52:41 -0400

    Chore: Update formatting

--- pkgs/gameglass/update.sh
+++ pkgs/gameglass/update.sh
@@ -8,7 +8,7 @@ VERSION="$(curl -s https://download.gameglass.gg/hub/latest-linux.yml | yq -r '.
 url="https://download.gameglass.gg/hub/GameGlass.AppImage"
 # If it's the same as current, skip other steps
 if [ "$VERSION" == "$(nix eval .\#gameglass.version --raw)" ]; then
-        exit 0
+  exit 0
 fi
 
 HASH="$(nix store prefetch-file "$url" --name "GameGlass.AppImage" --json | jq -r .hash)"
@@ -16,7 +16,7 @@ HASH="$(nix store prefetch-file "$url" --name "GameGlass.AppImage" --json | jq -
 # echo "{\"version\": \"$VERSION\", \"hash\": \"$SRI_HASH\" }" | jq >./pkgs/gameglass/sources.json
 
 jq -n \
-        --arg version "$VERSION" \
-        --arg url "$url" \
-        --arg hash "$HASH" \
-        '{version: $version, url: $url, hash: $hash}' >"$INFO"
+  --arg version "$VERSION" \
+  --arg url "$url" \
+  --arg hash "$HASH" \
+  '{version: $version, url: $url, hash: $hash}' >"$INFO"
