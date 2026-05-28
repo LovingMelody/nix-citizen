69dd7f69e2b36f65874bf370ee21c303c9db5c68
Melody Renata <me@lovingmelody.io>
2026-05-28 12:34:50 -0400

    chore: add quick option to cache builds

--- update-script.sh
+++ update-script.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash
-#! nix-shell -p bash nix-update npins curl gnused gnugrep jaq
-nix-update --flake lug-helper
-# RSI Launcher
-./pkgs/rsi-launcher/update.sh
-# GameGlass
-./pkgs/gameglass/update.sh
-# lug-wine-bin
-./pkgs/lug-wine-bin/update.sh
+#! nix-shell -p bash nix-update npins curl gnused gnugrep jaq cachix
+if [ "${1:-}" != "--cache-only" ]; then
+        nix-update --flake lug-helper
+        # RSI Launcher
+        ./pkgs/rsi-launcher/update.sh
+        # GameGlass
+        ./pkgs/gameglass/update.sh
+        # lug-wine-bin
+        ./pkgs/lug-wine-bin/update.sh
 
-# Compattools
-./pkgs/steamcompattools/update.sh
+        # Compattools
+        ./pkgs/steamcompattools/update.sh
 
-npins update
+        npins update
 
-# Update wine-astral sources
-# This has to be done after pins are updated
-./pkgs/wine-astral/update.sh
+        # Update wine-astral sources
+        # This has to be done after pins are updated
+        ./pkgs/wine-astral/update.sh
 
-nix fmt
+        nix fmt
+fi
+
+if [ "${1:-}" = "--cache" ] || [ "${1:-}" = "--cache-only" ]; then
+        nom build .#rsi-launcher .#rsi-launcher-git .#lug-helper .#gameglass .#rsi-launcher-umu .#lug-wine-bin .#proton-ge-bin .#dw-proton-bin .#proton-cachyos-bin .#proton-em-bin --print-out-paths --no-link --keep-going --refresh | cachix push nix-citizen
+        nom build .#packages.x86_64-linux-v3.rsi-launcher .#packages.x86_64-linux-v3.rsi-launcher-git .#packages.x86_64-linux-v3.lug-helper .#packages.x86_64-linux-v3.gameglass .#packages.x86_64-linux-v3.rsi-launcher-umu .#packages.x86_64-linux-v3.lug-wine-bin .#packages.x86_64-linux-v3.proton-ge-bin .#packages.x86_64-linux-v3.dw-proton-bin .#packages.x86_64-linux-v3.proton-cachyos-bin .#packages.x86_64-linux-v3.proton-em-bin --print-out-paths --no-link --keep-going --refresh | cachix push nix-citizen
+fi
