51dee80d92848983c96e9a04b05487267c653e63
Melody Renata <me@lovingmelody.io>
2026-07-12 07:08:51 -0400

    Update script: cleanup logic, use nom if available

--- update-script.sh
+++ update-script.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash
 #! nix-shell -p bash nix-update npins curl gnused gnugrep jaq cachix
+
 if [ "${1:-}" != "--cache-only" ]; then
   nix-update --flake lug-helper
+
   # RSI Launcher
   ./pkgs/rsi-launcher/update.sh
   # GameGlass
@@ -15,34 +17,38 @@ if [ "${1:-}" != "--cache-only" ]; then
 
   npins update
 
-  # Update wine-astral sources
-  # This has to be done after pins are updated
+  # Update wine-astral sources (after pins are updated)
   ./pkgs/wine-astral/update.sh
 
   nix fmt
 fi
 
+nix_build() {
+  NIX="nix"
+  if command -v nom >/dev/null; then
+    NIX="nom"
+  fi
+
+  "$NIX" build -L --print-out-paths --no-link --keep-going --refresh "$@"
+}
+
 if [ "${1:-}" = "--cache" ] || [ "${1:-}" = "--cache-only" ]; then
-  nix build -L --print-out-paths --no-link --keep-going --refresh \
-    .#rsi-launcher \
-    .#rsi-launcher-git \
-    .#lug-helper \
-    .#gameglass \
-    .#rsi-launcher-umu \
-    .#lug-wine-bin \
-    .#proton-ge-bin \
-    .#dw-proton-bin \
-    .#proton-cachyos-bin \
-    .#proton-em-bin | cachix push nix-citizen &&
-    nix build --print-out-paths --no-link --keep-going --refresh \
-      .#packages.x86_64-linux-v3.rsi-launcher \
-      .#packages.x86_64-linux-v3.rsi-launcher-git \
-      .#packages.x86_64-linux-v3.lug-helper \
-      .#packages.x86_64-linux-v3.gameglass \
-      .#packages.x86_64-linux-v3.rsi-launcher-umu \
-      .#packages.x86_64-linux-v3.lug-wine-bin \
-      .#packages.x86_64-linux-v3.proton-ge-bin \
-      .#packages.x86_64-linux-v3.dw-proton-bin \
-      .#packages.x86_64-linux-v3.proton-cachyos-bin \
-      .#packages.x86_64-linux-v3.proton-em-bin | cachix push nix-citizen
+  logFile="$(mktemp --suffix '.nix_updater_output')"
+
+  pkgs=(
+    'rsi-launcher' 'rsi-launcher-git' 'lug-helper' 'gameglass' 'rsi-launcher-umu'
+    'lug-wine-bin' 'proton-ge-bin' 'dw-proton-bin' 'proton-cachyos-bin' 'proton-em-bin'
+  )
+
+  targets=()
+  optimizedTargets=()
+
+  for pkg in "${pkgs[@]}"; do
+    targets+=(".#$pkg")
+    optimizedTargets+=(".#packages.x86_64-linux-v3.$pkg")
+  done
+
+  nix_build "${targets[@]}" >"$logFile" && nix_build "${optimizedTargets[@]}" >>"$logFile"
+  cachix push nix-citizen <"$logFile"
+  trap "rm '$logFile'" exit
 fi
